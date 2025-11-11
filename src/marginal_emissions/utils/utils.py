dfabcace7a9c3f5b9b7504c50071ebcc5195e81a
aklei <a.kleinwaechter97@gmail.com>
2025-11-11 15:14:47 +0100

    Adding classes

--- src/marginal_emissions/utils/utils.py
+++ src/marginal_emissions/utils/utils.py
@@ -1,4 +1,18 @@
-def say_hello():
-    print("Hello from marginal_emissions!")
-    
+import chardet
+
+
+class Helpers:
+    def __init__(self, path):
+        self.path=path
+
+    def check_encoding(self, path):
+        with open(path, 'rb') as f:
+            raw = f.read(10000) # first 10 KB for encoding detection
+            encoding = chardet.detect(raw)['encoding']
+
+        return encoding['encoding'].to
+
+
+    def say_hello(self):
+        print("Hello from marginal_emissions!")
 
